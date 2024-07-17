33401d1fedeba19c936011b2da4a7b3f7b1184a7
Laura Keidann Rodrigues da Silva <82679218+laurakrs@users.noreply.github.com>
2024-07-17 19:31:05 -0300

    Change answer noise to 0.01

--- trab2/src/analysis.py
+++ trab2/src/analysis.py
@@ -21,7 +21,7 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.0
+    answerNoise = 0.01
     return answerDiscount, answerNoise
 
 # Prefer the close exit (+1), risking the cliff (-10)
