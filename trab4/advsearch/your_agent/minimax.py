6ce81924c9303d486844f92a0e5277229f7f5b35
Lais Canabarro <laisaraujo1312@gmail.com>
2024-08-08 19:09:46 -0300

    add files

---
+++ trab4/advsearch/your_agent/minimax.py
@@ -0,0 +1,16 @@
+import random
+from typing import Tuple, Callable
+
+
+
+def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
+    """
+    Returns a move computed by the minimax algorithm with alpha-beta pruning for the given game state.
+    :param state: state to make the move (instance of GameState)
+    :param max_depth: maximum depth of search (-1 = unlimited)
+    :param eval_func: the function to evaluate a terminal or leaf state (when search is interrupted at max_depth)
+                    This function should take a GameState object and a string identifying the player,
+                    and should return a float value representing the utility of the state for the player.
+    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    """
+    raise NotImplementedError()
