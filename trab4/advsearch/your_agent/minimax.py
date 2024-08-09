8cc5f2cf3476a5521a9da073bcbe05fc67ef6018
Beatriz Soviero <79771418+biasoviero@users.noreply.github.com>
2024-08-09 19:51:05 -0300

    Update minimax.py

--- trab4/advsearch/your_agent/minimax.py
+++ trab4/advsearch/your_agent/minimax.py
@@ -13,4 +13,39 @@ def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
                     and should return a float value representing the utility of the state for the player.
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-    raise NotImplementedError()
+    _, a = max(state, float('-inf'), float('inf'), max_depth, eval_func)
+    return a
+
+def max(state, alfa, beta, max_depth:int, eval_func:Callable):
+    if state.is_terminal() or max_depth == 0:
+        return eval_func(state, state.get_current_player())
+    v = float('-inf')
+    a = None
+    for action in state.legal_moves():
+        new_state = state.copy()
+        new_state.perform_action(action)
+        val = min(new_state, alfa, beta, max_depth-1, eval_func)
+        if val > v:
+            v = val
+            a = action
+        alfa = max(alfa, v)
+        if beta <= alfa:
+            break
+    return v, a
+
+def min(state, alfa, beta, max_depth:int, eval_func:Callable):
+    if state.is_terminal() or max_depth == 0:
+        return eval_func(state, state.get_current_player())
+    v = float('inf')
+    a = None
+    for action in state.legal_moves():
+        new_state = state.copy()
+        new_state.perform_action(action)
+        val = max(new_state, alfa, beta, max_depth-1, eval_func)
+        if val < v:
+            v = val
+            a = action
+        beta = min(beta, v)
+        if beta <= alfa:
+            break
+    return v, a
