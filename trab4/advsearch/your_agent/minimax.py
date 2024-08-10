935053d86ff8a13878fb713100966a3d3e041050
Beatriz Soviero <79771418+biasoviero@users.noreply.github.com>
2024-08-09 22:39:11 -0300

    questões 2.1 e 2.1.1

--- trab4/advsearch/your_agent/minimax.py
+++ trab4/advsearch/your_agent/minimax.py
@@ -1,4 +1,3 @@
-import random
 from typing import Tuple, Callable
 
 
@@ -13,18 +12,20 @@ def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
                     and should return a float value representing the utility of the state for the player.
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-    _, a = max(state, float('-inf'), float('inf'), max_depth, eval_func)
+    if max_depth == -1:
+        max_depth = float('inf')
+
+    _, a = maxGain(state, float('-inf'), float('inf'), state.player, max_depth, eval_func)
     return a
 
-def max(state, alfa, beta, max_depth:int, eval_func:Callable):
+def maxGain(state, alfa, beta, player, max_depth:int, eval_func:Callable):
     if state.is_terminal() or max_depth == 0:
-        return eval_func(state, state.get_current_player())
+        return eval_func(state, player), None
     v = float('-inf')
     a = None
     for action in state.legal_moves():
-        new_state = state.copy()
-        new_state.perform_action(action)
-        val = min(new_state, alfa, beta, max_depth-1, eval_func)
+        new_state = state.next_state(action)
+        val, _ = minGain(new_state, alfa, beta, player, max_depth-1, eval_func)
         if val > v:
             v = val
             a = action
@@ -33,15 +34,14 @@ def max(state, alfa, beta, max_depth:int, eval_func:Callable):
             break
     return v, a
 
-def min(state, alfa, beta, max_depth:int, eval_func:Callable):
+def minGain(state, alfa, beta, player, max_depth:int, eval_func:Callable):
     if state.is_terminal() or max_depth == 0:
-        return eval_func(state, state.get_current_player())
+        return eval_func(state, player), None
     v = float('inf')
     a = None
     for action in state.legal_moves():
-        new_state = state.copy()
-        new_state.perform_action(action)
-        val = max(new_state, alfa, beta, max_depth-1, eval_func)
+        new_state = state.next_state(action)
+        val, _ = maxGain(new_state, alfa, beta, player, max_depth-1, eval_func)
         if val < v:
             v = val
             a = action
--- trab4/advsearch/your_agent/tttm_minimax.py
+++ trab4/advsearch/your_agent/tttm_minimax.py
@@ -1,7 +1,5 @@
-import random
 from typing import Tuple
 from ..tttm.gamestate import GameState
-from ..tttm.board import Board
 from .minimax import minimax_move
 
 # Voce pode criar funcoes auxiliares neste arquivo
@@ -25,10 +23,16 @@ def make_move(state: GameState) -> Tuple[int, int]:
     # uma vez que o jogo tem profundidade maxima 9. 
     # Preencha a funcao utility com o valor de um estado terminal e passe-a como funcao de avaliação para seu minimax_move
 
-    return random.choice(range(3)), random.choice(range(3))
+    return minimax_move(state, -1, utility)
 
 def utility(state, player:str) -> float:
     """
     Retorna a utilidade de um estado (terminal) 
     """
-    return 0   # substitua pelo seu codigo
+    if state.winner() == player:
+        return 1
+    elif state.winner() == None:
+        return 0
+    else:
+        return -1
+    
