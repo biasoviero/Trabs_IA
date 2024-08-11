2169661a59f5df3974cdd81721c86762c7499206
Beatriz Soviero <79771418+biasoviero@users.noreply.github.com>
2024-08-10 22:31:56 -0300

    questoes 2.1.2.1 e 2.1.2.2

--- trab4/advsearch/your_agent/othello_minimax_count.py
+++ trab4/advsearch/your_agent/othello_minimax_count.py
@@ -23,7 +23,7 @@ def make_move(state) -> Tuple[int, int]:
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, 64, evaluate_count)
 
 
 def evaluate_count(state, player:str) -> float:
@@ -34,4 +34,4 @@ def evaluate_count(state, player:str) -> float:
     :param state: state to evaluate (instance of GameState)
     :param player: player to evaluate the state for (B or W)
     """
-    return 0   # substitua pelo seu codigo
+    return state.get_board().num_pieces(player) - state.get_board().num_pieces(Board.opponent(player))
--- trab4/advsearch/your_agent/othello_minimax_mask.py
+++ trab4/advsearch/your_agent/othello_minimax_mask.py
@@ -37,7 +37,7 @@ def make_move(state) -> Tuple[int, int]:
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, 64, evaluate_mask)
 
 
 def evaluate_mask(state, player:str) -> float:
@@ -49,4 +49,11 @@ def evaluate_mask(state, player:str) -> float:
     :param state: state to evaluate (instance of GameState)
     :param player: player to evaluate the state for (B or W)
     """
-    return 0   # substitua pelo seu codigo
+    value = 0
+    for i in range(8):
+        for j in range(8):
+            if state.get_board().tiles[i][j] == player:
+                value += EVAL_TEMPLATE[i][j]
+            elif state.get_board().tiles[i][j] == Board.opponent(player):
+                value -= EVAL_TEMPLATE[i][j]
+    return value
